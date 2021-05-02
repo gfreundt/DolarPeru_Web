a365b75df59c26f56fe82a3e57695bad86f686d1
Gabriel Freundt <gfreundt@gmail.com>
2021-05-02 11:44:52 -0500

    Fixed Template Directory

--- app.py
+++ app.py
@@ -6,19 +6,23 @@ import time
 
 
 def find_path():
-    paths = (r'C:\Users\Gabriel Freundt\Google Drive\Multi-Sync\sharedData\data',r"D:\Google Drive Backup\Multi-Sync\sharedData\data", r"C:\users\gfreu\Google Drive\Multi-Sync\sharedData\data")
+    paths = (r'C:\Users\Gabriel Freundt\Google Drive\Multi-Sync\sharedData',r"D:\Google Drive Backup\Multi-Sync\sharedData", r"C:\users\gfreu\Google Drive\Multi-Sync\sharedData")
     for path in paths:
         if os.path.exists(path):
             return path
 
-MAIN_PATH = find_path()
+main_path = find_path()
+DATA_PATH = os.path.join(main_path, 'data')
+HTML_PATH = os.path.join(main_path, 'templates')
+
+print(HTML_PATH)
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=HTML_PATH)
 
 @app.route("/dog")
 def index():
-	with open(os.path.join(MAIN_PATH, 'TDC_fixed.txt'), mode='r') as file:
+	with open(os.path.join(DATA_PATH, 'TDC_fixed.txt'), mode='r') as file:
 		latest = [i for i in file.readlines()]
 		return render_template('index.html', latest=latest)
 
@@ -28,3 +32,4 @@ if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
 
 
+#D:\Google Drive Backup\Multi-Sync\sharedData\templates
