4bced492d55457696aeee085c354f490a3d9001c
Gabriel laptop <gfreundt@gmail.com>
2021-08-24 10:56:18 -0500

    Added stats page and contact info

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from  google.cloud import storage
 def which_system():
 	systems = [{'name': 'GFT-Tablet', 'root_path': r'C:\pythonCode'},
 		 	   {'name': 'raspberrypi', 'root_path': r'/home/pi/pythonCode'},
-			   {'name': 'Power', 'root_path': r'D:\pythonCode'},
+			   {'name': 'laptop', 'root_path': r'C:\pythonCode'},
 			   {'name': 'Ubuntu-gft', 'root_path': '/home/gabriel/pythonCode'},
 			   {'name': 'Google Cloud App Engine', 'root_path': '/home/gabfre/pythonCode'}]
 	for system in systems:
@@ -15,7 +15,7 @@ def which_system():
 	return systems[-1]['name'], systems[-1]['root_path']
 
 
-def construct_data(filename):
+def get_data_from_bucket(filename):
 	# Access Google Cloud Storage Bucket
 	if SYSTEM != 'Google Cloud App Engine': 
 		client = storage.Client.from_service_account_json(json_credentials_path=GCLOUD_KEYS)
@@ -24,28 +24,38 @@ def construct_data(filename):
 	bucket = client.get_bucket(GCLOUD_BUCKET)
 	file_in_bucket = bucket.blob('/DolarPeru_data/' + filename)
 	data = json.loads(file_in_bucket.download_as_string().decode('utf-8'))
-	details = data['details']
-	details1, details2 = details[:len(details)//2], details[len(details)//2:]
-	return data, details1, details2
+	return data
+
+def split_in_two(details):
+	return details[:len(details)//2], details[len(details)//2:]
+
+
 
 
 SYSTEM, ROOT_PATH = which_system()
 GCLOUD_KEYS = os.path.join(ROOT_PATH, 'gcloud_keys.json')
-GCLOUD_BUCKET = 'data-bucket-gft-devops'   # testing = 'data-bucket-gft-devops' | production = 'data-bucket-gft'
+GCLOUD_BUCKET = 'data-bucket-gft'   # testing = 'data-bucket-gft-devops' | production = 'data-bucket-gft'
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/venta")
+@app.route('/')
+@app.route('/venta')
 def venta():
-	data, details1, details2 = construct_data('WEB_Venta.json')
+	data = get_data_from_bucket('WEB_Venta.json')
+	details1, details2 = split_in_two(data['details'])
 	return render_template('venta.html', head=data['head'], details1=details1, details2=details2)
 
-@app.route("/compra")
+@app.route('/compra')
 def compra():
-	data, details1, details2 = construct_data('WEB_Compra.json')
+	data = get_data_from_bucket('WEB_Compra.json')
+	details1, details2 = split_in_two(data['details'])
 	return render_template('compra.html', head=data['head'], details1=details1, details2=details2)
 
+@app.route('/stats')
+def stats():
+	data = get_data_from_bucket('stats.json')
+	print(data)
+	return render_template('stats.html', meta=data['Meta'], results=data['Results'])
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
