35ce8897d42ccbd0fa4462e9f9ff49444f1746f1
Gabriel <gfreundt@gmail.com>
2021-10-17 13:11:57 -0500

    v2 app

--- main-old.py
+++ main-old.py
@@ -8,7 +8,7 @@ def which_system():
         {"name": "GFT-Tablet", "root_path": r"C:\pythonCode"},
         {"name": "raspberrypi", "root_path": r"/home/pi/pythonCode"},
         {"name": "laptop", "root_path": r"C:\pythonCode"},
-        {"name": "power", "root_path": r"D:\pythonCode"},
+        {"name": "Ubuntu-gft", "root_path": "/home/gabriel/pythonCode"},
         {"name": "Google Cloud App Engine", "root_path": "/home/gabfre/pythonCode"},
     ]
     for system in systems:
@@ -32,7 +32,7 @@ def get_data_from_bucket(filename):
 
 
 def get_data_from_file(filename):
-    with open(filename, "r") as file:
+    with open("c:/pythonCode/DolarPeru_data/" + filename, "r") as file:
         data = json.loads(file.read())
     return data
 
@@ -42,9 +42,6 @@ def split_in_two(details):
 
 
 SYSTEM, ROOT_PATH = which_system()
-DATA_PATH = os.path.join(ROOT_PATH, "DolarPeru_data")
-GRAPH_PATH = os.path.join(DATA_PATH, "graphs")
-WEBFILE_PATH = os.path.join(DATA_PATH, "webfiles")
 GCLOUD_KEYS = os.path.join(ROOT_PATH, "gcloud_keys.json")
 GCLOUD_BUCKET = "data-bucket-gft"  # testing = 'data-bucket-gft-devops' | production = 'data-bucket-gft'
 
@@ -54,8 +51,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/venta")
 def venta():
-    data = get_data_from_file(os.path.join(DATA_PATH, "web000.json"))["venta"]
-    details1, details2 = split_in_two(data["incluidos"])
+    data = get_data_from_bucket("WEB_Venta.json")
+    details1, details2 = split_in_two(data["details"])
     return render_template(
         "venta.html", head=data["head"], details1=details1, details2=details2
     )
@@ -63,8 +60,8 @@ def venta():
 
 @app.route("/compra")
 def compra():
-    data = get_data_from_file(os.path.join(DATA_PATH, "web000.json"))["compra"]
-    details1, details2 = split_in_two(data["incluidos"])
+    data = get_data_from_bucket("WEB_Compra.json")
+    details1, details2 = split_in_two(data["details"])
     return render_template(
         "compra.html", head=data["head"], details1=details1, details2=details2
     )
@@ -72,25 +69,19 @@ def compra():
 
 @app.route("/stats")
 def stats():
-    data = get_data_from_file(os.path.join(DATA_PATH, "stats.json"))
-    return render_template(
-        "stats.html",
-        meta=data["meta"],
-        activity=data["activity"],
-        scraper_results=data["scraper_results"],
-    )
+    data = get_data_from_bucket("stats.json")
+    return render_template("stats.html", meta=data["Meta"], results=data["Results"])
 
 
 @app.route("/fintech/<path:id>")
 def fintech(id):
-    print(id, WEBFILE_PATH)
-    data = get_data_from_file(os.path.join(WEBFILE_PATH, "webfile-" + id + ".json"))
+    data = get_data_from_file("WEB_" + id + ".json")
     return render_template(
         "fintech.html",
         id=id,
         datos=data["datos"],
-        ultima=data["cotizaciones"]["vigente"][0],
-        antiguas=data["cotizaciones"]["historicas"],
+        ultima=data["cotizaciones"][-1],
+        antiguas=data["cotizaciones"][:-1],
     )
 
 
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def which_system():
         {"name": "GFT-Tablet", "root_path": r"C:\pythonCode"},
         {"name": "raspberrypi", "root_path": r"/home/pi/pythonCode"},
         {"name": "laptop", "root_path": r"C:\pythonCode"},
-        {"name": "Ubuntu-gft", "root_path": "/home/gabriel/pythonCode"},
+        {"name": "power", "root_path": r"D:\pythonCode"},
         {"name": "Google Cloud App Engine", "root_path": "/home/gabfre/pythonCode"},
     ]
     for system in systems:
@@ -32,7 +32,7 @@ def get_data_from_bucket(filename):
 
 
 def get_data_from_file(filename):
-    with open("c:/pythonCode/DolarPeru_data/" + filename, "r") as file:
+    with open(filename, "r") as file:
         data = json.loads(file.read())
     return data
 
@@ -42,6 +42,9 @@ def split_in_two(details):
 
 
 SYSTEM, ROOT_PATH = which_system()
+DATA_PATH = os.path.join(ROOT_PATH, "DolarPeru_data")
+GRAPH_PATH = os.path.join(DATA_PATH, "graphs")
+WEBFILE_PATH = os.path.join(DATA_PATH, "webfiles")
 GCLOUD_KEYS = os.path.join(ROOT_PATH, "gcloud_keys.json")
 GCLOUD_BUCKET = "data-bucket-gft"  # testing = 'data-bucket-gft-devops' | production = 'data-bucket-gft'
 
@@ -51,8 +54,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/venta")
 def venta():
-    data = get_data_from_bucket("WEB_Venta.json")
-    details1, details2 = split_in_two(data["details"])
+    data = get_data_from_file(os.path.join(WEBFILE_PATH, "webfile-000.json"))["venta"]
+    details1, details2 = split_in_two(data["incluidos"])
     return render_template(
         "venta.html", head=data["head"], details1=details1, details2=details2
     )
@@ -60,8 +63,8 @@ def venta():
 
 @app.route("/compra")
 def compra():
-    data = get_data_from_bucket("WEB_Compra.json")
-    details1, details2 = split_in_two(data["details"])
+    data = get_data_from_file(os.path.join(WEBFILE_PATH, "webfile-000.json"))["compra"]
+    details1, details2 = split_in_two(data["incluidos"])
     return render_template(
         "compra.html", head=data["head"], details1=details1, details2=details2
     )
@@ -69,19 +72,25 @@ def compra():
 
 @app.route("/stats")
 def stats():
-    data = get_data_from_bucket("stats.json")
-    return render_template("stats.html", meta=data["Meta"], results=data["Results"])
+    data = get_data_from_file(os.path.join(DATA_PATH, "stats.json"))
+    return render_template(
+        "stats.html",
+        meta=data["meta"],
+        activity=data["activity"],
+        scraper_results=data["scraper_results"],
+    )
 
 
 @app.route("/fintech/<path:id>")
 def fintech(id):
-    data = get_data_from_file("WEB_" + id + ".json")
+    print(id, WEBFILE_PATH)
+    data = get_data_from_file(os.path.join(WEBFILE_PATH, "webfile-" + id + ".json"))
     return render_template(
         "fintech.html",
         id=id,
         datos=data["datos"],
-        ultima=data["cotizaciones"][-1],
-        antiguas=data["cotizaciones"][:-1],
+        ultima=data["cotizaciones"]["vigente"][0],
+        antiguas=data["cotizaciones"]["historicas"],
     )
 
 
